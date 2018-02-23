7141dcebc0fa47bd8102ecb32a229476ee01902a
Saykongthong <saykongthong.22@hotmail.com>
2018-02-23 11:55:24 +0700

    Connection to DB

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,13 +1,21 @@
 package sample;
 
+import admin.adminController;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable{
@@ -38,5 +46,39 @@ public class Controller implements Initializable{
         }else{
             this.dbStatus.setText("Not Connect to DB.");
         }
-    }
+    }//initialii
+    @FXML
+    public  void  Login(ActionEvent event){
+        try{
+            if (this.loginmodel.isLogin(username.getText(),password.getText())){
+                Stage stage = (Stage) this.btnlogin.getScene().getWindow();
+                stage.close();
+                adminDashborad();
+
+            }else {
+                loginStatus.setText("Your username or password is inva");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }//isLonig
+
+    private void adminDashborad() {
+        try{
+            Stage adminStage = new Stage();
+            FXMLLoader adminLoader = new FXMLLoader();
+            Pane adminRoot = (Pane) adminLoader.load(getClass().getResource("/admin/adminDashboard.fxml").openStream());
+            adminController admincontroller = adminLoader.getController();
+            Scene scene = new Scene(adminRoot);
+            adminStage.setScene(scene);
+            adminStage.setTitle("Admin Dashborad");
+            adminStage.setResizable(false);
+            adminStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }//adminDashborad
 }//class
